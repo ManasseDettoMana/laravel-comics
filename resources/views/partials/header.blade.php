0f6daf9f1c8d89b534adb521c1e50b234664f437
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-04 15:28:34 +0100

    installazione bootstrap e "sistemazione" header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,20 @@
 <header>
-    <div class="container flex-center">
-        <figure class="flex-center">
-            <img src="{{asset('images/dc-logo.png')}}" alt="">
-        </figure>
-        <ul class="flex-center">
-            @foreach($links as $link)
-                <li class="{{ request()->routeIs($link['route']) ? 'active' : ''}}"><a href="{{$link['route']}}" >{{$link['text']}}</a></li>
-            @endforeach
-        </ul>
-
+    <div class="container">
+        <div class="row">
+            <figure class="col-4 ">
+                <a href="#">
+                    <img src="{{asset('images/dc-logo.png')}}" alt="">
+                </a>
+            </figure>
+            <div class="col-8 " id="header_col_links">
+                <nav class="">
+                    <ul class="d-flex">
+                        @foreach($links as $link)
+                            <li class="{{ request()->routeIs($link['route']) ? 'active' : ''}}"><a href="{{$link['route']}}" >{{$link['text']}}</a></li>
+                        @endforeach
+                    </ul>
+                </nav>
+            </div>
+        </div>
     </div>
 </header>
