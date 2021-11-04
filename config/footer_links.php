1410cf472c2c05927694e4ad476e992d077f098c
ManasseDettoMana <smarrazzomanasse@gmail.com>
2021-11-04 17:48:45 +0100

    config footer links

---
+++ config/footer_links.php
@@ -0,0 +1,39 @@
+<?php
+    return[
+        $dc_comuics = [
+
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#']
+        ],
+        $shop = [
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#']
+
+        ],
+        $dc = [
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#']
+        ],
+        $sites = [
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#'],
+            ['text'=>'Lorem', 'src'=>'#']
+        ]
+    ]
+?>
